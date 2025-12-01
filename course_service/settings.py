878d4ad755165e0689272fc4e4bced72567aa25c
salaheddinekassouri <yacinesalahkassouri@gmail.com>
2025-12-02 00:02:51 +0100

    COMMIT 4

--- course_service/settings.py
+++ course_service/settings.py
@@ -75,14 +75,7 @@ WSGI_APPLICATION = 'course_service.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
